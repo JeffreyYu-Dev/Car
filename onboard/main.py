faec79837e30f231d87c7d621e3747471d9242d3
jeffrey yu <jeffrey.yu.w@gmail.com>
2026-07-28 23:40:43 -0400

    first commit

---
+++ onboard/main.py
@@ -0,0 +1,7 @@
+# notes for now
+# cold start -> ask the driver for details
+# monitor driving(acceleration, breaking, speed) for driver profile
+# monitor sensor data in a buffer theres no need to store in over time, only necessary to record maybe 5 minutes before the problem happens and store that
+# if sensor detects something abnormal trigger llm sequence
+# create vehicle query using sensor and driver profile data
+# send it off to the backend
